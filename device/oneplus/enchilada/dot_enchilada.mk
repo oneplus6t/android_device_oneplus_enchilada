7e032e7515d40bbbeef0dc2434110518a52c6807
android-build-team Robot <android-build-team-robot@google.com>
2019-06-20 01:34:53 +0000

    dot

---
+++ device/oneplus/enchilada/dot_enchilada.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from enchilada device
+$(call inherit-product, device/oneplus/enchilada/device.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Boos4721"
+DOT_BUILD_TYPE= OFFICIAL
+
+PRODUCT_NAME := dot_enchilada
+PRODUCT_DEVICE := enchilada
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := ONEPLUS A6003
+PRODUCT_RELEASE_NAME := enchilada
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus6
+TARGET_VENDOR_DEVICE_NAME := OnePlus6
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus6 \
+    PRODUCT_NAME=OnePlus6 \
+    PRIVATE_BUILD_DESC="OnePlus6-user 9 PKQ1.180716.001 1905281230 release-keys"
+
+BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:9/PKQ1.180716.001/1905281230:user/release-keys
