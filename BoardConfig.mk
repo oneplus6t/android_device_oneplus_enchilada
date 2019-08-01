f2487d706cfc661888d8cd2c8228f8c038ffcc5d
Moon <3.1415926535boos@gmail.com>
2019-08-01 17:19:33 +0800

    enchilada: rebelliopn ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@
 
 DEVICE_PATH := device/oneplus/enchilada
 
+#SElinux
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
